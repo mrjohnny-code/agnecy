1a72281a166019a32a93c3f71dd9588c3ad26851
vanek <vanek2105@gmail.com>
2019-10-21 16:20:34 +0000

    adding files

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+	  sass = require('gulp-sass'),
+	  sourcemaps = require('gulp-sourcemaps'),
+	  autoprefixer = require('gulp-autoprefixer'),
+	  browserSync = require('browser-sync'),
+	  watch = require('gulp-watch');
+
+// Style
+gulp.task('sass', function() {
+	return gulp.src('./scss/style.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest('./css/'))
+	.pipe(browserSync.stream())
+});
+
+// Watch
+gulp.task('serve', function() {
+	browserSync.init({
+		notify: false,
+		server: "./"
+	});
+
+	gulp.watch('./scss/**/*.scss', gulp.series('sass'))
+	gulp.watch('*.html').on('change', () => {
+		browserSync.reload();
+	});
+});
+
+gulp.task('default', gulp.series('sass', 'serve'));
